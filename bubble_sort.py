ddd9c57b751336ad8ef461106624d2e26fe49f3b
WiseAngel <wiseangelgames@gmail.com>
2021-08-11 15:35:42 +0300

    ref: 1. Алгоритм сортировки пузырьком 2. Алгоритм сортировки пузырьком в документе-справке

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,13 +2,13 @@ import random
 arr = random.sample(range(0, 20), 10)
 print (arr)
 
-def bubble_sort(arr):
-    len_list = len(arr)
-    for i in range(len_list):
-        for j in range(len_list - i -1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    print(arr)
+def bubble_sort(a):
+    N = len(a)
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+    print(a)
 
 bubble_sort(arr)
 
